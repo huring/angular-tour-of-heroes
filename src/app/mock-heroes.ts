74f0a8f89527561a27684e5d4bf4e6d40dfa8539
Lars Huring <lars.huring@gmail.com>
2017-12-14 16:19:29 +0100

    Added detail component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Mr Nice'},
+  {id: 12, name: 'Fantasticman'},
+  {id: 13, name: 'Beast'},
+  {id: 14, name: 'Boy boy'},
+  {id: 15, name: 'Girl'},
+  {id: 16, name: 'Greatorus'},
+  {id: 17, name: 'Compuserve'},
+  {id: 18, name: 'Coolman'},
+  {id: 19, name: 'Icegirl'},
+  {id: 20, name: 'Batboy'},
+  {id: 11, name: 'Spaceinvader'},
+  {id: 11, name: 'Dr Alienz'},
+]
