e6cd34a5131f82b15fb0e53f9a025c049857cebf
Lars Huring <lars.huring@gmail.com>
2017-12-15 09:55:18 +0100

    .

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  getHeroes(): Observable<Hero[]>  {
+    this.messageService.add('Heroservice: Fetched hero');
+    return of(HEROES);
+  }
+
+  constructor(private messageService: MessageService) { }
 
 }
